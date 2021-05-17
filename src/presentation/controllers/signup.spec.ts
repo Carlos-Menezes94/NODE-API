03af06f339830913561ee5c066e67a30e20987d5
Carlos Menezes <menezes.carloseng@gmail.com>
2021-05-17 14:48:45 -0300

    test: ensure SignUpControler returns 400 if no password is provided and some corrections

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,31 +1,45 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
 describe('SignUp Controller', () => {
-    test('Should return 400 if no name provided', () => {
+    test('Should return 400 if no name is provided', () => {
         const sut = new SignUpController ()
         const httpRequest = {
             body:{
                 email: 'any_email@mail.com',
-                passoword: 'any_passowrod',
-                passodwordConfirmation: 'any_passoword'
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
             }
         }
         const httpResponse = sut.handle(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
+        expect (httpResponse.statusCode).toBe(400)
         expect (httpResponse.body).toEqual (new MissingParamError('name'))
     })
 
-    test('Should return 400 if no email provided', () => {
+    test('Should return 400 if no email is provided', () => {
         const sut = new SignUpController ()
         const httpRequest = {
             body:{
                 name: 'any_name',
-                passoword: 'any_passowrod',
-                passodwordConfirmation: 'any_passoword'
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
             }
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-        expect (httpResponse.body).toEqual (new MissingParamError('mail'))
+        expect (httpResponse.body).toEqual (new MissingParamError('email'))
+    })
+    test('Should return 400 if no password is provided', () => {
+        const sut = new SignUpController ()
+        const httpRequest = {
+            body:{
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect (httpResponse.body).toEqual (new MissingParamError('password'))
     })
   })
