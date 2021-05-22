9ff1433fb7dc746c19bdc5d809774b6691c6fdcf
Carlos Menezes <menezes.carloseng@gmail.com>
2021-05-21 21:42:39 -0300

    test: ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,46 +1,53 @@
 import { SignUpController } from './signup'
 import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { EmailValidator } from '../protocols'
+import { AccountModel } from '../../domain/models/account'
+import { AddAccount, AddAccountModel } from '../../domain/usercase/add-account'
+
+const makeEmailValidator = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+        isValid (email: string): boolean {
+            return true
+        }
+    }
+    return new EmailValidatorStub ()    
+}
+
+const makeAddAccount = (): AddAccount => {
+    class AddAccountStub implements AddAccount {
+        add (account: AddAccountModel): AccountModel {
+            const fakeAccount = {
+                id: 'valid_id',
+                name: 'valid_name',
+                email: 'valid_email@mail.com',
+                password: 'valid_password'
+            }
+            return fakeAccount 
+        }
+    }
+    return new AddAccountStub ()
+}  
+
 interface SutTypes {
     sut: SignUpController
     emailValidatorStub: EmailValidator
-}
-
-const makeEmailValidator = (): EmailValidator =>{
-    class EmailValidatorStub implements EmailValidator {
-        isValid (email: string): boolean {
-            return true
-        }
+    addAccountStub: AddAccount
     }
-    return new EmailValidatorStub ()
-}
-
-const makeEmailValidatorWithError = (): EmailValidator =>{
-    class EmailValidatorStub implements EmailValidator {
-        isValid (email: string): boolean {
-            throw new Error ()
-        }
-    }
-    return new EmailValidatorStub ()
-}
 
 const makeSut = (): SutTypes => {
-    class EmailValidatorStub implements EmailValidator {
-        isValid (email: string): boolean {
-            return true
-        }
-    }
     const emailValidatorStub = makeEmailValidator ()
-    const sut = new SignUpController (emailValidatorStub)
+    const addAccountStub = makeAddAccount()
+    const sut = new SignUpController(emailValidatorStub, addAccountStub)
     return {
         sut,
-        emailValidatorStub
-    }    
+        emailValidatorStub,
+        addAccountStub
+    }
 }
 
 describe('SignUp Controller', () => {
     test('Should return 400 if no name is provided', () => {
-        const { sut } = makeSut ()
+        const { sut } = makeSut()
         const httpRequest = {
             body:{
                 email: 'any_email@mail.com',
@@ -82,7 +89,7 @@ describe('SignUp Controller', () => {
         expect (httpResponse.body).toEqual (new MissingParamError('password'))
     })
 
-    test('Should return 400 if no passwordConfirmation is provided', () => {
+    test('Should return 400 if no password confirmation is provided', () => {
         const { sut } = makeSut ()
         const httpRequest = {
             body:{
@@ -95,6 +102,22 @@ describe('SignUp Controller', () => {
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect (httpResponse.body).toEqual (new MissingParamError('passwordConfirmation'))
+    })
+  
+    test('Should return 400 if password confirmation fails', () => {
+        const { sut } = makeSut ()
+        const httpRequest = {
+            body:{
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'invalid_password'
+            }
+        }
+        
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect (httpResponse.body).toEqual (new InvalidParamError('passwordConfirmation'))
     })
 
     test('Should return 400 if an invalid email is provided', () => {
@@ -116,7 +139,7 @@ describe('SignUp Controller', () => {
     
     test('Should call EmailValidator with correct email', () => {
         const { sut, emailValidatorStub } = makeSut ()
-        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
         const httpRequest = {
             body:{
                 name: 'any_name',
@@ -128,12 +151,27 @@ describe('SignUp Controller', () => {
         sut.handle(httpRequest)
         expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
     })
+
     test('Should return 500 if EmailValidator throws', () => {
-    
-        const { sut, emailValidatorStub } = makeSut ()
-        jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce (() => {
-            throw new Error ()            
+        const { sut, emailValidatorStub} = makeSut ()
+        jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() =>{
+            throw new Error()
         })
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe (500)
+        expect(httpResponse.body).toEqual (new ServerError())
+    })
+    test('Shoult call AddAccount with correct values', () => {
+        const { sut, addAccountStub } = makeSut ()
+        const addSpy = jest.spyOn(addAccountStub, 'add')
         const httpRequest = {
             body:{
                 name: 'any_name',
@@ -142,9 +180,11 @@ describe('SignUp Controller', () => {
                 passwordConfirmation: 'any_password'
             }
         }
-        const httpResponse = sut.handle(httpRequest)
-        expect(httpResponse.statusCode).toBe(500)
-        expect (httpResponse.body).toEqual (new ServerError())
-
-    }) 
-  })
+        sut.handle(httpRequest)
+        expect(addSpy).toHaveBeenCalledWith({
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            password: 'any_password'
+        })
+    })
+})
