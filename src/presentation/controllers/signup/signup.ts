387554264d0811ab3d18b3a96761880d3b6b0c77
Carlos Menezes <menezes.carloseng@gmail.com>
2021-05-21 22:46:07 -0300

    refactor: transform handle to async

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -11,7 +11,7 @@ export class SignUpController implements Controller {
         this.addAccount = addAccount    
     }
 
-    handle (httpRequest: httpRequest): httpResponse {
+    async handle (httpRequest: httpRequest): Promise<httpResponse> {
         try {    
             const requiredFileds = ['name', 'email', 'password', 'passwordConfirmation']
             for (const field of requiredFileds) {
